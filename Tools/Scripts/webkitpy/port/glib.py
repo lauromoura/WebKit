d27a8b66092176810e64b20a05337c562a1e66a3
Lauro Moura <lmoura@igalia.com>
2023-04-02 23:26:40 -0300

    [GLIB] Tools: Fix typo setting up glib environment

--- Tools/Scripts/webkitpy/port/glib.py
+++ Tools/Scripts/webkitpy/port/glib.py
@@ -142,7 +142,7 @@ class GLibPort(Port):
         env = os.environ.copy()
         env['WEBKIT_EXEC_PATH'] = self._build_path('bin')
         env['WEBKIT_INJECTED_BUNDLE_PATH'] = self._build_path('lib')
-        env['LD_LIBRARY_PATH'] = self._prepend_to_env_value(self._build_path('lib'), env.get('LD_LIBRAY_PATH', ''))
+        env['LD_LIBRARY_PATH'] = self._prepend_to_env_value(self._build_path('lib'), env.get('LD_LIBRARY_PATH', ''))
         return env
 
     def _get_crash_log(self, name, pid, stdout, stderr, newer_than, target_host=None):
